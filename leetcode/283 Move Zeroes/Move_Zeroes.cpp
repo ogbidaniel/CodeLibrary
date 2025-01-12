45d09c5cbb2acc0bf695aaf6edf2329341b7fd99
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2025-01-11 23:57:22 -0600

    move zerroes

---
+++ leetcode/283 Move Zeroes/Move_Zeroes.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <iomanip> // For formatting output
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0, right = 0;
+
+        while (right < nums.size()) {
+            // Visualization: Print the current state
+            cout << "Array: ";
+            for (int i = 0; i < nums.size(); ++i) {
+                if (i == left && i == right) {
+                    cout << "[" << nums[i] << "] ";
+                } else if (i == left) {
+                    cout << "(" << nums[i] << ") ";
+                } else if (i == right) {
+                    cout << "{" << nums[i] << "} ";
+                } else {
+                    cout << nums[i] << " ";
+                }
+            }
+            cout << "\nLeft Index: " << left << ", Value: " << nums[left] 
+                 << "\nRight Index: " << right << ", Value: " << nums[right] 
+                 << "\n\n";
+
+            // Process the current element
+            if (nums[right] != 0) {
+                swap(nums[left], nums[right]);
+                left++;
+            }
+            right++;
+        }
+    }
+};
+
+int main() {
+    vector<int> nums = {0, 1, 0, 3, 12};
+    Solution sol;
+
+    cout << "Initial Array:\n";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << "\n\nStarting Move Zeroes Visualization:\n\n";
+    sol.moveZeroes(nums);
+
+    cout << "\nFinal Array:\n";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
